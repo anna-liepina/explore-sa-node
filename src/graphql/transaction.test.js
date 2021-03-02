e1f17e73eeb3a7eb76d39c2908864067836b4687
Anna Liepina <anna.liepina@yahoo.co.uk>
2021-03-02 23:09:49 +0000

    tests: graphql transaction resolver

--- src/graphql/transaction.test.js
+++ src/graphql/transaction.test.js
@@ -1,26 +1,65 @@
-describe.skip('GraphQL: Transaction', () => {
+describe('GraphQL: Transaction', () => {
     describe('query', () => {
-        it(`xxx`, async () => {
+        it(`should fetch transaction by ID`, async () => {
             const { data } = await query({
                 query: `
                 {
+                    transaction(id: 1) {
+                        id
+                        price
+                        date
+                    }
                 }`
             });
 
             expect(data).toMatchSnapshot();
         });
 
-    });
+        describe('paginated queries', () => {
+            it(`search without date range`, async () => {
+                const { data } = await query({
+                    query: `
+                    {
+                        transactionSearch(perPage: 5) {
+                            id
+                            price
+                            date
+                        }
+                    }`
+                });
 
-    describe('mutation', () => {
-        it(`xxx`, async () => {
-            const { data, errors } = await query({
-                query: `
-                mutation {
-                }`
+                expect(data).toMatchSnapshot();
             });
 
-            expect(data).toMatchSnapshot();
+            it(`search in data range, first page, 5 per page`, async () => {
+                const { data } = await query({
+                    query: `
+                    {
+                        transactionSearch(from: "2010-01-01", to: "2021-01-01", perPage: 5) {
+                            id
+                            price
+                            date
+                        }
+                    }`
+                });
+
+                expect(data).toMatchSnapshot();
+            });
+
+            it(`search in data range, seconds page, 5 per page`, async () => {
+                const { data } = await query({
+                    query: `
+                    {
+                        transactionSearch(from: "2010-01-01", to: "2021-01-01", page: 2, perPage: 5) {
+                            id
+                            price
+                            date
+                        }
+                    }`
+                });
+
+                expect(data).toMatchSnapshot();
+            });
         });
     });
 });
