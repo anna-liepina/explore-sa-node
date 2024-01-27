240094b0994852654413c3ea088c0345278d8913
Anna Liepina <47822694+anna-liepina@users.noreply.github.com>
2024-01-27 15:27:36 +0000

    review parse:properties (#157)
    * review parse:properties
    
    * fix typo

--- src/parse:properties.ts
+++ src/parse:properties.ts
@@ -155,9 +155,6 @@ if (!fs.existsSync(file)) {
         
     const outputProcessingInfo = (final?: boolean) => {
         output.sections[1] = output.processingInfo(processedRecords, processedInvalidRecords, transactions.length, queue, final);
-    }
-
-    const outputDuplicateTransactions = () => {
         output.sections[2] = [
             Output.line,
             ' extra information',
@@ -168,7 +165,6 @@ if (!fs.existsSync(file)) {
     }
 
     outputProcessingInfo();
-    outputDuplicateTransactions();
 
     for await (const row of parser) {
         const postcode = row[3];
@@ -215,15 +211,14 @@ if (!fs.existsSync(file)) {
             }
         }
 
-        const transactionHash = `${price}|${date}`;
-
+        const transactionHash = `${date}|${price}`;
         if (!propertiesStore.get(guid).has(transactionHash)) {
             propertiesStore.get(guid).add(transactionHash);
 
             transactions.push({
                 guid,
-                price,
                 date,
+                price,
             });
         } else {
             duplicateTransactions++;
@@ -238,7 +233,6 @@ if (!fs.existsSync(file)) {
             queue.add(persist(orm.Transaction, [...transactions]));
 
             outputProcessingInfo();
-            outputDuplicateTransactions();
 
             markers.length = 0;
             properties.length = 0;
@@ -266,7 +260,6 @@ if (!fs.existsSync(file)) {
     queue.add(persist(orm.Property, properties));
     queue.add(persist(orm.Transaction, transactions));
     outputProcessingInfo(true);
-    outputDuplicateTransactions();
 
     if (!dryRun) {
         output.sections.push([
